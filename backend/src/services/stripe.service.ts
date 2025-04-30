a3c51ab4dff954b9aeaaa6255a3510da54f8b63d
Dileep <dileep.samala@connect.qut.edu.au>
2025-04-30 20:33:46 +1000

    Add stripe service

---
+++ backend/src/services/stripe.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import Stripe from 'stripe';
+
+@Injectable()
+export class StripeService {
+  private readonly stripe: Stripe;
+
+  constructor() {
+    this.stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+      apiVersion: '2025-03-31.basil',
+      typescript: true,
+    });
+  }
+
+  async createCheckoutSession(
+    priceId: string,
+    userEmail?: string,
+  ): Promise<Stripe.Checkout.Session> {
+    return this.stripe.checkout.sessions.create({
+      line_items: [
+        {
+          price: priceId,
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      success_url: `${process.env.CLIENT_URL}/success`,
+      cancel_url: `${process.env.CLIENT_URL}/cancel`,
+      customer_email: userEmail,
+    });
+  }
+}

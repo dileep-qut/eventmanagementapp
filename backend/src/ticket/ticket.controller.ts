66a2f59581047b754a9363d244ab4bc53fc5ef9b
marcusnguyen <marcus.kimlong@gmail.com>
2025-05-13 23:20:57 +1000

    Update find my tickets

--- backend/src/ticket/ticket.controller.ts
+++ backend/src/ticket/ticket.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Request } from '@nestjs/common';
+import { Body, Controller, Get, Post, Request } from '@nestjs/common';
 import { TicketService } from './ticket.service';
 import { GetTicketPriceDto } from './dto/get-ticket-price.dto';
 import { ApiBearerAuth, ApiResponse } from '@nestjs/swagger';
@@ -65,5 +65,36 @@ export class TicketController {
     if (response.uri) {
       return { url: response.uri };
     }
+  }
+
+  @ApplyStrictAuth(true)
+  @ApplyApiResponse([400, 401, 403, 500])
+  @ApiResponse({
+    status: 200,
+    description: 'Find my tickets',
+    schema: {
+      example: [
+        {
+          _id: '682345d4c36f1bd8acc37ac0',
+          event_id: {
+            _id: '68218e24f41de91e2e46ed0a',
+            name: 'Event 1',
+            description: 'Description 1',
+            location: 'Location 1',
+            start_time: '2026-01-24T14:00:00.000Z',
+            end_time: '2026-01-25T14:00:00.000Z',
+          },
+          checked_in: false,
+          transaction_id: 'dummy_transaction_id',
+          createdAt: '2025-05-13T13:15:00.193Z',
+          updatedAt: '2025-05-13T13:15:00.193Z',
+          __v: 0,
+        },
+      ],
+    },
+  })
+  @Get('find-my-tickets')
+  async findMyTicket(@Request() req) {
+    return await this.ticketService.findMyTickets(req.user._id);
   }
 }

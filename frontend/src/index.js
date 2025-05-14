176f7ddfddf99c3987993cd72907aa720871da17
Marcus Nguyen <87351566+MarcusNguyenDev@users.noreply.github.com>
2025-05-14 23:04:01 +1000

    Hotfix FE (#41)
    * Hotfix index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,33 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import '@mantine/core/styles.css';
-import { MantineProvider } from '@mantine/core';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
 
-import '@mantine/core/styles.css';
+import "@mantine/core/styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-import '@mantine/core/styles.css';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-
-import '@mantine/core/styles.css';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-     
     <MantineProvider withGlobalStyles withNormalizeCSS>
-      
       <App />
-      
     </MantineProvider>
-   
   </React.StrictMode>
 );
-

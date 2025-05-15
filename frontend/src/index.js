173e662d08725c30785050c8887d52ccb7b8e66f
Naigal Roy <naighu@Jobros-MacBook-Air.local>
2025-05-15 18:12:05 +1000

    Toast message added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import '@mantine/core/styles.css';
+import '@mantine/notifications/styles.css';
 import { MantineProvider } from '@mantine/core';
+import { Notifications } from '@mantine/notifications';
+
 
 import '@mantine/core/styles.css';
 
@@ -23,7 +26,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
      
     <MantineProvider withGlobalStyles withNormalizeCSS>
-      
+      <Notifications position="top-right"/>
       <App />
       
     </MantineProvider>
